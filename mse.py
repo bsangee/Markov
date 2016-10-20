ab74f0bc5e245483a339988f59e92fc6bd930dc9
Sangeetha <bsangee@vt.edu>
2016-10-20 12:31:46 -0400

    Update mse.py

--- mse.py
+++ mse.py
@@ -21,13 +21,16 @@ print pred
 y_true.append(np.float(text[3]))
 y_pred.append(np.float(pred[0]))
 mse.append(mean_squared_error(y_true, y_pred))
-for i in xrange(4,8,2):
-        pred =chained.markov_chain(text[:i],order)
-        y_true=[]
-        y_pred=[]
-        y_true.append([float(s) for s in (text[i+1:(2*i - 1)])])
-        y_pred.append([float(s) for s in (pred[:i-2])])
-        mse.append(mean_squared_error(y_true, y_pred))
+print 'len' + str(len(text))
+for j in xrange(4, len(text), 8):
+        for i in xrange(j,9,2):
+                pred =chained.markov_chain(text[:i],order)
+                y_true=[]
+                y_pred=[]
+                y_true.append([float(s) for s in (text[i+1:(2*i - 1)])])
+                y_pred.append([float(s) for s in (pred[:i-2])])
+                mse.append(mean_squared_error(y_true, y_pred))
+                print 'i' + str(i) + 'j ' + str(j)
 
 
 print 'Optimal Length:' + str( (mse.index(min(mse)) * 2 + 2) )
