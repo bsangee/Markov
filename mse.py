ef7cb2b78d3957e0dac15b68cb22bad9ba3ba7aa
Sangeetha <bsangee@vt.edu>
2016-10-20 12:22:39 -0400

    Optimal No. of Requests
    Number of requests for which we obtain least MSE (only requests of length 2, 4 and 8 are considered)

--- mse.py
+++ mse.py
@@ -1,4 +1,4 @@
-#Find mean square error of strings of lengths 2, 4 and 8 
+#Number of requests for which we obtain least MSE (only requests of length 2, 4 and 8 are considered)
 from parse import parser
 import states
 import chained
@@ -9,6 +9,7 @@ from sklearn.metrics import mean_squared_error
 order = 2
 y_true=[]
 y_pred=[]
+mse = []
 
 p = parser()
 p.parse_file()  #File name can be changed in parse.py
@@ -19,11 +20,14 @@ pred =chained.markov_chain(text[:2],order)
 print pred
 y_true.append(np.float(text[3]))
 y_pred.append(np.float(pred[0]))
-print mean_squared_error(y_true, y_pred)
+mse.append(mean_squared_error(y_true, y_pred))
 for i in xrange(4,8,2):
         pred =chained.markov_chain(text[:i],order)
         y_true=[]
         y_pred=[]
         y_true.append([float(s) for s in (text[i+1:(2*i - 1)])])
         y_pred.append([float(s) for s in (pred[:i-2])])
-        print mean_squared_error(y_true, y_pred)
+        mse.append(mean_squared_error(y_true, y_pred))
+
+
+print 'Optimal Length:' + str( (mse.index(min(mse)) * 2 + 2) )
