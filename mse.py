7e4d195cdc0fa60b7509601cfaa98bcf7116f82c
Sangeetha <bsangee@vt.edu>
2016-10-20 11:48:46 -0400

    Update mse.py

--- mse.py
+++ mse.py
@@ -1,5 +1,4 @@
-#still working on this
-
+#IN PPROGRESS
 from parse import parser
 import states
 import chained
@@ -21,20 +20,18 @@ y_pred.append(np.float(pred[0]))
 print mean_squared_error(y_true, y_pred)
 
 pred =chained.markov_chain(text[:4],order)
-y_true.append(np.float(text[5:7]))
-y_pred.append(np.float(pred[:2]))
+y_true=[]
+y_pred=[]
+y_true.append([float(s) for s in (text[5:7])])
+y_pred.append([float(s) for s in (pred[:2])])
 print mean_squared_error(y_true, y_pred)
-
 
 pred =chained.markov_chain(text[:8],order)
-
-
-y_true.append(np.float(text[9:13]))
-y_pred.append(np.float(pred[:4]))
+y_true=[]
+y_pred=[]
+y_true.append([float(i) for i in (text[9:13])])
+y_pred.append([float(i) for i in (pred[:4])])
 print mean_squared_error(y_true, y_pred)
 
-pred =chained.markov_chain(text[:16],order)
-y_true.append(np.float(text[17:25]))
-y_pred.append(np.float(pred[:8]))
-print mean_squared_error(y_true, y_pred)
 
+#print pred
