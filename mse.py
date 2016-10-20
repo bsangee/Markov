d9e177a67b2d23315892965031552089122a7730
Sangeetha <bsangee@vt.edu>
2016-10-20 12:15:19 -0400

    Update mse.py

--- mse.py
+++ mse.py
@@ -1,4 +1,4 @@
-#IN PPROGRESS
+#Find mean square error of strings of lengths 2, 4 and 8 
 from parse import parser
 import states
 import chained
@@ -14,24 +14,16 @@ p = parser()
 p.parse_file()  #File name can be changed in parse.py
 text = p.write_bytes
 text = states.write_bytes_states(text)
+
 pred =chained.markov_chain(text[:2],order)
+print pred
 y_true.append(np.float(text[3]))
 y_pred.append(np.float(pred[0]))
 print mean_squared_error(y_true, y_pred)
-
-pred =chained.markov_chain(text[:4],order)
-y_true=[]
-y_pred=[]
-y_true.append([float(s) for s in (text[5:7])])
-y_pred.append([float(s) for s in (pred[:2])])
-print mean_squared_error(y_true, y_pred)
-
-pred =chained.markov_chain(text[:8],order)
-y_true=[]
-y_pred=[]
-y_true.append([float(i) for i in (text[9:13])])
-y_pred.append([float(i) for i in (pred[:4])])
-print mean_squared_error(y_true, y_pred)
-
-
-#print pred
+for i in xrange(4,8,2):
+        pred =chained.markov_chain(text[:i],order)
+        y_true=[]
+        y_pred=[]
+        y_true.append([float(s) for s in (text[i+1:(2*i - 1)])])
+        y_pred.append([float(s) for s in (pred[:i-2])])
+        print mean_squared_error(y_true, y_pred)
